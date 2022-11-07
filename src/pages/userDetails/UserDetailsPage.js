68338df594c0d0fa6fb755ad7f5e3aa475503087
Caleno-R <calenorose@gmail.com>
2022-11-07 22:29:54 +0300

    feat(user details): set up user details page
    To set up user details arrow function component

---
+++ src/pages/userDetails/UserDetailsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+
+const UserDetails = () => {
+    const navigate = useNavigate();
+
+  return (
+    <div>UserDetails</div>
+  )
+}
+
+export default UserDetails;
