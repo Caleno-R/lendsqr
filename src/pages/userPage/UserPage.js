3f533c4918cebae698eece7c9ab4c52fad5c0e24
Caleno-R <calenorose@gmail.com>
2022-11-07 22:31:10 +0300

    feat(user page): set up user page
    To set up user page arrow function component

---
+++ src/pages/userPage/UserPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+
+const UserPage = () => {
+    const navigate = useNavigate();
+
+  return (
+    <div>UserPage</div>
+  )
+}
+
+export default UserPage;
