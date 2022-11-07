934a2c0aaa31894db4f5fe0cf8e1d5fd587ffa77
Caleno-R <calenorose@gmail.com>
2022-11-07 22:06:15 +0300

    feat(login page): set up the login page
    Set up login page with an arrow function

---
+++ src/pages/auth/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+    const navigate = useNavigate();
+
+  return (
+    <div>LoginPage</div>
+  );
+}
+
+export default LoginPage;
+
