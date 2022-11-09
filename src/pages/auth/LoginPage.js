81914f7ca78c593c310498fca9d995f0c6a9d028
Caleno-R <calenorose@gmail.com>
2022-11-09 23:36:26 +0300

    feat(login): add login functionality

--- src/pages/auth/LoginPage.js
+++ src/pages/auth/LoginPage.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import "../../styles/loginSheet.css";
 
 const LoginPage = () => {
     const navigate = useNavigate();
 
   return (
-    <div>LoginPage</div>
-  );
-}
+    <div className="login-container">
+      <div className="login-logo-loti">
+        <div className="">
+        </div>
+        <div>
+        </div>
+      </div>
+      <div className="login-head-form">
+        <div className="login-head-info">
+          <h3 className="login-head">Welcome!</h3>
+          <p className="login-info">Enter details to login.</p>
+        </div>
+        <div  className="login-form">
+          <form>
+            <div className="form-email">
+              <input type="email" placeholder="Email" className="login-input"/>
+            </div>
+            <div className="form-password">
+              <input type="password" placeholder="Password" className="login-input"/>
+              <div>show</div>
+            </div>
+            <p><Link to="" className="forgot-password">forgot password?</Link></p>
+            <div className="">
+              <button type="submit" className="login-btn btn-text">log in</button>
+            </div> 
+          </form>
+        </div>
+      </div>
+    </div>
 
+  );
+
+}
 export default LoginPage;
 
