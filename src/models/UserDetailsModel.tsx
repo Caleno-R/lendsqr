825f1a3458b63df9f8aa71d6e7b6b9f9da0ad73d
Caleno-R <calenorose@gmail.com>
2022-11-14 00:01:01 +0300

    feat(user details models): user's detail

---
+++ src/models/UserDetailsModel.tsx
@@ -0,0 +1,10 @@
+export interface UserContainerDetail{
+    fullName: string;
+    userId: string;
+    userTier: number;
+    bankBalance: number;
+    bankName: string;
+    bankAccountNumber: string;
+    avatarPic?: string;
+    currency: string;
+}
