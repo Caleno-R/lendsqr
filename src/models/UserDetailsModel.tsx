953589a2330177d8707eb1692c8fdece39279ac5
Caleno-R <calenorose@gmail.com>
2022-11-15 00:32:59 +0300

    feat(user response interface): create response interface for users request

--- src/models/UserDetailsModel.tsx
+++ src/models/UserDetailsModel.tsx
@@ -8,3 +8,53 @@ export interface UserContainerDetail{
     avatarPic?: string;
     currency: string;
 }
+
+export interface UserDetailsResponse extends UsersMetaResponse{
+    profile: ProfileResponse;
+    guarantor: GuarantorResponse;
+    socials: SocialsResponse;
+    education: EducationResponse;
+}
+
+export interface UsersMetaResponse{
+    createdAt: string;
+    orgName: string;
+    userName: string;
+    email: string;
+    phoneNumber: string;
+    lastActiveDate: string;
+    accountBalance: string;
+    accountNumber: string;
+    id: string;
+}
+
+export interface ProfileResponse extends GuarantorResponse{
+    avatar: string;
+    bvn: string;
+    currency: string;
+}
+
+export interface GuarantorResponse{
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    gender: string;
+    address: string;
+}
+
+export interface SocialsResponse{
+    facebook: string;
+    instagram: string;
+    twitter: string;
+}
+
+export interface EducationResponse{
+    level: string;
+    employmentStatus: string;
+    sector: string;
+    duration: string;
+    officeEmail: string;
+    monthlyIncome: Array<string>;
+    loanRepayment: string;
+}
+
