e805cdcce7f91e932c13ad59cdf2b2a952866d67
Caleno-R <calenorose@gmail.com>
2022-11-15 00:26:27 +0300

    feat(index store): create configuration for app stores

--- src/stores/index.tsx
+++ src/stores/index.tsx
@@ -1,9 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./AuthStore";
+import educationSlice from "./educationStore";
+import guarantorSlice from "./guarantorStore";
+import profileSlice from "./profileStore";
+import socialSlice from "./socialsStores";
+import usersMetaSlice from "./usersStore";
 
 export const store = configureStore({
     reducer: {
         auth: authSlice.reducer,
+        profile: profileSlice.reducer,
+        social: socialSlice.reducer,
+        education: educationSlice.reducer,
+        guarantor: guarantorSlice.reducer,
+        usersMeta: usersMetaSlice.reducer,
     }
 });
 
